bc01a2c2dac83ab1a17ec0908624bad17644a696
Leo <leogonglin@gmail.com>
2015-01-22 22:40:14 +0800

    initialization

---
+++ src/main/java/com/leo/test/imperative/ImperativeTest.java
@@ -0,0 +1,15 @@
+package com.leo.test.imperative;
+
+/**
+ * Created by leo on 15/1/22.
+ */
+public class ImperativeTest {
+
+    public static void main(String[] args) {
+        int i = 0;
+        i = i++;
+//        i = ++i;
+//        i = i++ + i++ + ++i;
+        System.out.println(i);
+    }
+}
